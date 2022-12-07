de0e8138b05ba0866b0f28afbe1a9199f184df58
myedunote <43767464+myedunote@users.noreply.github.com>
2022-12-07 11:45:32 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,10 +1,12 @@
 export default {
-  async fetch(request, env) {
-    const _url = new URL(request.url);
-    _url.hostname = _url.pathname.startsWith("/gh/")
-      ? "cdn.jsdelivr.net"
-      : "www.baidu.com";
-    const req = new Request(_url, request);
-    return fetch(req);
-  },
-};
+    async fetch(request, env) {
+      let url = new URL(request.url);
+      if (url.pathname.startsWith('/')) {
+        url.hostname="example.com";
+        let new_request=new Request(url,request);
+        return fetch(new_request);
+      }
+      // Otherwise, serve the static assets.
+      return env.ASSETS.fetch(request);
+    }
+  };
