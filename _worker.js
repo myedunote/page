9ca37f773110b8d02a8e9423d80918113621b5c3
myedunote <43767464+myedunote@users.noreply.github.com>
2022-12-13 10:59:36 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="hostloc.com";
+        url.hostname="www.v2ex.com";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
